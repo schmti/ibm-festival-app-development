6f38e1322977496743b193bbc8d19c76b9fdc1c5
Tim Schmidt <schmidt@viada.de>
2021-10-20 14:25:09 +0200

    quick

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "I´ve done some new coooooooool features!!!";
+		return "I have done some new coooooooool features!!!";
 	}
 
 }
