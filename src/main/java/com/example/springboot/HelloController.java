723b99bc6e91ac039bdbe213006928b916bdf406
Tim Schmidt <schmidt@viada.de>
2021-10-20 14:22:21 +0200

    some cool new fetaures

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "I´ve done some new coooooooool features!!!";
 	}
 
 }
